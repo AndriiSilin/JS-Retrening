8a45ff0c6d8cc2cf83455ad61d874d0df8f86445
AndriiSilin <118901812+AndriiSilin@users.noreply.github.com>
2022-11-29 11:33:30 +0200

    Add files via upload
    HomeWork1

---
+++ index.js
@@ -0,0 +1,60 @@
+//Task 2 
+//var lastName = "Silin";
+//console.log(lastName);
+
+//task3 
+//var firstName = "Andrii";
+//var lastName = "Silin";
+//lastName = firstName;
+//console.log(firstName, lastName);
+
+//task4
+//const student = {
+//    firtName: "Andrii",
+ //   age: 35,
+ //   men: true,
+ //   weight: null,
+ //   yearOfGraduate: undefined
+//} 
+//console.log(student);
+
+//task5
+//var isAdult = confirm("How  old are you?");
+//alert(isAdult);
+//console.log(isAdult);
+
+//task6
+//   const student ={
+//    firstName: "Andrii",
+//   lastName: "Silin",
+//    group: "LV-732",
+//    birthdayYear: 1984,
+//    marriedStatus: true,
+//    pets: undefined, 
+//    children: null,
+//}
+//console.log(typeof student.firstName, 
+//    typeof student.lastName, 
+//    typeof student.group, 
+//    typeof student.birthdayYear, 
+//    typeof student.marriedStatus,
+//    typeof student.pets, 
+//   typeof student.children);
+
+//console.log(student.birthdayYear,
+//    student.marriedStatus,
+//    student.firstName, 
+//    student.lastName, 
+//    student.group);
+
+//task7
+//var email = prompt("Your email");
+//var userName = prompt("Your User name");
+//var password = prompt("Your password");
+//alert('Account has been created \n Your email - ' + email + '\n Your Log In ID - ' + userName + '\n Your password - ' + password);
+
+//task8
+//var hours = +prompt("How many hours you need to complete task?");
+//var days = +prompt("How many days you need to complete task?");
+//days = days*24
+//alert('You will spend: \n' + (days + hours) + ' hours');
